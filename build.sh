748fd456431817c232221c652ea90da884ef3aa4
mksachs <mike@mikesachs.com>
2025-06-30 13:52:56 -0700

    Early 2025 Version

--- build.sh
+++ build.sh
@@ -1,62 +1,78 @@
 #!/bin/zsh
 source ~/.zshrc
 
-pyenv activate mikesachs.com
+
 
 BASEDIR=$(pwd)
 
-#
-# Create the web version of the PDF resume.
-#
-cd "$BASEDIR"/python/
-python build_content.py -w
-cd "$BASEDIR"/tex/
-/Library/TeX/texbin/xelatex -synctex=1 -interaction=nonstopmode -output-driver="/Library/TeX/texbin/xdvipdfmx -q -E" "$BASEDIR"/tex/pdf.ltx
-mv "$BASEDIR"/tex/pdf.pdf "$BASEDIR"/pdf/MichaelSachs.pdf
+OPTSTRING="rc:"
 
-#
-# Create the mini web version of the PDF resume.
-#
-#cd "$BASEDIR"/python/
-#python build_content.py -w -m
-#cd "$BASEDIR"/tex/
-#/Library/TeX/texbin/xelatex -synctex=1 -interaction=nonstopmode -output-driver="/Library/TeX/texbin/xdvipdfmx -q -E" "$BASEDIR"/tex/pdf_web.xetex
-#mv "$BASEDIR"/tex/pdf_web.pdf "$BASEDIR"/pdf/MichaelSachsMini.pdf
+while getopts ${OPTSTRING} opt; do
+    case ${opt} in
+        r)
+            echo "######### Create a PDF version of the resume #########"
+
+            pyenv deactivate
+
+            pyenv activate mikesachs.com
+
+            cd "$BASEDIR"/python/
+            python build_content.py -w
+            cd "$BASEDIR"/tex/
+            /Library/TeX/texbin/xelatex -synctex=1 -interaction=nonstopmode -output-driver="/Library/TeX/texbin/xdvipdfmx -q -E" "$BASEDIR"/tex/pdf.ltx
+            mv "$BASEDIR"/tex/pdf.pdf "$BASEDIR"/pdf/MichaelSachs.pdf
+
+            cd "$BASEDIR"
+
+            #
+            # Cleanup
+            #
+            rm tex/*.aux
+            rm tex/*.log
+            rm tex/*.out
+            rm tex/*.synctex.gz
+            rm tex/*.ltx
+            rm tex/*.log
+
+            pyenv deactivate
+            ;;
+        c)
+            echo "######### Create a cover letter called ${OPTARG} #########"
+
+            cd "$BASEDIR"/tex/
+            cp "$BASEDIR"/tex/templates/cover_letter_boilerplate.ltx "$BASEDIR"/tex/cover_letter_tmp.ltx
+            /Library/TeX/texbin/xelatex -synctex=1 -interaction=nonstopmode -output-driver="/Library/TeX/texbin/xdvipdfmx -q -E" "$BASEDIR"/tex/cover_letter_tmp.ltx
+            mv "$BASEDIR"/tex/cover_letter_tmp.pdf "$HOME"/Documents/cover_letters/"${OPTARG}".pdf
+
+            cd "$BASEDIR"
+            #
+            # Cleanup
+            #
+            rm tex/*.aux
+            rm tex/*.log
+            rm tex/*.out
+            rm tex/*.synctex.gz
+            rm tex/*.ltx
+            rm tex/*.log
+
+            ;;
+        :)
+            echo "Option -${OPTARG} requires an argument."
+            exit 1
+            ;;
+        ?)
+            echo "Invalid option: -${OPTARG}."
+            exit 1
+            ;;
+    esac
+done
+
+
 #
 ##
-## Create the print version of the PDF resume.
+## Synch with the server.
 ##
-#cd "$BASEDIR"/python/
-#python build_content.py -p
-#cd "$BASEDIR"/tex/
-#/Library/TeX/texbin/xelatex -synctex=1 -interaction=nonstopmode -output-driver="/Library/TeX/texbin/xdvipdfmx -q -E" "$BASEDIR"/tex/pdf_print.xetex
-#mv "$BASEDIR"/tex/pdf_print.pdf "$BASEDIR"/pdf/MichaelSachsPrint.pdf
 #
-##
-## Create the mini print version of the PDF resume.
-##
-#cd "$BASEDIR"/python/
-#python build_content.py -p -m
-#cd "$BASEDIR"/tex/
-#/Library/TeX/texbin/xelatex -synctex=1 -interaction=nonstopmode -output-driver="/Library/TeX/texbin/xdvipdfmx -q -E" "$BASEDIR"/tex/pdf_print.xetex
-#mv "$BASEDIR"/tex/pdf_print.pdf "$BASEDIR"/pdf/MichaelSachsPrintMini.pdf
+##rsync -avhL --delete --exclude-from 'exclude-list.txt' "$BASEDIR"/ mksachs@iad1-shared-b8-17.dreamhost.com
 
-cd "$BASEDIR"
 
-#
-# Cleanup
-#
-rm tex/*.aux
-rm tex/*.log
-rm tex/*.out
-rm tex/*.synctex.gz
-rm tex/*.ltx
-rm tex/*.log
-
-#
-# Synch with the server.
-#
-
-#rsync -avhL --delete --exclude-from 'exclude-list.txt' "$BASEDIR"/ mksachs@iad1-shared-b8-17.dreamhost.com
-
-pyenv deactivate
